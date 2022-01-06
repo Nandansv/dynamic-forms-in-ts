aad3d8b15812c1f7ad783cc264f875120f1836b9
Nandan  Mac <nandansvamc1993@hotmail.com>
2022-01-07 01:22:08 +0530

    Latest changes to consider config

--- src/DynamicFormDDF.tsx
+++ src/DynamicFormDDF.tsx
@@ -3,18 +3,49 @@ import { render } from 'react-dom';
 import FormRenderer from '@data-driven-forms/react-form-renderer/form-renderer';
 import FormTemplate from '@data-driven-forms/mui-component-mapper/form-template';
 import componentMapper from '@data-driven-forms/mui-component-mapper/component-mapper';
-
+import accountConfig from './config/Account.json';
 const schema = {
+    "title": "New Account Creation Form",
+    "description": "Enter Details",
     "fields": [
+      {
+        "component": "select",
+        "label": "Account Type",
+        "name": "accountType",
+        "options" : accountConfig.AccountEntities.map(e => ({
+            label: e.Name,
+            value: e.Id
+        })),
+      },
       {
         "component": "text-field",
         "label": "Name",
-        "name": "name"
+        "name": "name",
+        FormFieldGridProps: { xs: 6},
       },
       {
         "component": "text-field",
         "label": "Legal Business Name",
-        "name": "legalBusinessName"
+        "name": "legalBusinessName",
+        FormFieldGridProps: { xs: 5.9, sx: {marginLeft : '10px'} },
+      },
+      {
+        "component": "text-field",
+        "label": "Tax ID",
+        "name": "taxId",
+        FormFieldGridProps: { xs: 6 },
+      },
+      {
+        "component": "text-field",
+        "label": "Short Description",
+        "name": "shortDescription",
+        FormFieldGridProps: { xs: 5.9, sx: {marginLeft : '10px'}  },
+      },
+      {
+        "component": "textarea",
+        "label": "Long Description",
+        "name": "longDescription",
+        
       }
     ]
   };
